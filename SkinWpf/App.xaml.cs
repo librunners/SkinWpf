7ba60b0885ea91e8e81efb6188f4744f640ee74d
librunners <librunners@gmail.com>
2019-08-30 09:37:05 +0800

    添加项目文件。

---
+++ SkinWpf/App.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace SkinWpf
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        public App()
+        {
+            Initial();
+            this.Exit += new ExitEventHandler(App_Exit);
+        }
+
+        void App_Exit(object sender, ExitEventArgs e)
+        {
+            setFreeNotifyIcon();
+        }
+
+
+        #region 成员
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        #endregion
+
+        /// <summary>
+        /// 初始化托盘区图标
+        /// </summary>     
+        public void Initial()
+        {
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon();
+            this.notifyIcon.BalloonTipText = "欢迎";
+
+            this.notifyIcon.Icon = new System.Drawing.Icon("ShareWork.ico");
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new MouseEventHandler(notifyIcon_MouseDoubleClick);
+            this.notifyIcon.ShowBalloonTip(1000);
+            var menuItems = new MenuItem[3];
+            menuItems[0] = new MenuItem();
+            menuItems[0].Text = "显示主面板";
+            menuItems[0].Click += new EventHandler(nofityShow_Click);
+            menuItems[1] = new MenuItem();
+            menuItems[1].Text = "-";
+            menuItems[2] = new MenuItem();
+            menuItems[2].Text = "退出";
+            menuItems[2].Click += new EventHandler(notifyExit_Click);
+            menuItems[0].DefaultItem = true;
+            this.notifyIcon.ContextMenu = new ContextMenu(menuItems);
+        }
+
+
+        /// <summary>
+        /// 单击托盘区“显示”菜单事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void nofityShow_Click(object sender, EventArgs e)
+        {
+            //如果主窗体已隐藏
+            //for (int i = 0; i < App.Current.Windows.Count; i++)
+            //{
+            //    if (App.Current.Windows[i] is LoginWindow)
+            //    {
+            //        WindowExcute(App.Current.Windows[i]);
+            //        return;//截断
+            //    }
+            //}
+            WindowExcute(MainWindow);
+        }
+
+        /// <summary>
+        /// 托盘区图标双击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //for (int i = 0; i < App.Current.Windows.Count; i++)
+            //{
+            //    if (App.Current.Windows[i] is LoginWindow)
+            //    {
+            //        WindowExcute(App.Current.Windows[i]);
+            //        return;//截断
+            //    }
+            //}
+            WindowExcute(MainWindow);
+        }
+
+        /// <summary>
+        /// 窗口操作
+        /// </summary>
+        void WindowExcute(Window window)
+        {
+            //如果主窗体已隐藏
+            window.Visibility = Visibility.Visible;
+            window.WindowState = WindowState.Normal;
+            window.Activate();
+            window.Topmost = true;
+            window.Topmost = false;
+        }
+
+        /// <summary>
+        /// 单击托盘区“退出”菜单事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void notifyExit_Click(object sender, EventArgs e)
+        {
+            setFreeNotifyIcon();
+            Environment.Exit(0);
+        }
+        /// <summary>
+        /// 释放托盘区图标
+        /// </summary>
+        public void setFreeNotifyIcon()
+        {
+            this.notifyIcon.Visible = false;
+            this.notifyIcon.Dispose();
+        }
+    }
+}
---
+++ SkinWpf/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Panuon.UI.Silver;
+
+namespace SkinWpf
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+    
+        }
+        #region 界面元素操作
+        /// <summary>
+        /// 窗体移动
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) this.DragMove();
+        }
+        /// <summary>
+        /// 关闭窗体
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            //App.Current.Shutdown();
+            Environment.Exit(0);
+        }
+        /// <summary>
+        /// 窗体最小化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        #endregion
+      
+        /// <summary>
+        /// 点击启动成相室按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnStar_Click(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            menu.Show();
+        }
+    }
+}
